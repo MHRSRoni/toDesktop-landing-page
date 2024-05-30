f9a3d5e4b76c2dcb168e49be639d3229c67f8a81
MHRSRoni <mdmonirhosen.roni04@gmail.com>
2024-05-30 12:46:50 +0600

    [done] project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-7xl h-screen flex justify-center items-center bg-primary">
+      React tailwind with vite
+    </h1>
+    </>
+  )
+}
+
+export default App
